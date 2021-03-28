31c9aa8221a89f385e1d1b955d04bf0523bbd794
“Spencer <sbowles22@choate.edu>
2021-03-28 18:17:25 -0400

    Create GET for classes -s

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from typing import Optional
 
 from fastapi import FastAPI
+import json
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+data_fp = 'student_classes_data.json'
+with open(data_fp, "r") as read_file:
+    data = json.load(read_file)
+print(type(data['cbowles22@choate.edu']))
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Yo hey wassup try /classes/{email}"}
+
+
+@app.get("/classes/{email}")
+def read_classes(email: str):
+    return JSONResponse(content=json.dumps(data[email]))
 
 
 @app.get("/questions")
-def read_questions():
-    return {"classes": "EN300, Arcand"}
-
-
-@app.get("/admin")
 def read_questions():
     return {"data": "some data"}
